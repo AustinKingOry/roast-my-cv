96d2474ce830f602cc9bb789be83c1a3f93797bf
austinkings30 <austinkings30@gmail.com>
2025-08-05 13:44:41 +0000

    add constants file

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+export const siteSettings = {
+    paymentPlans: {
+        freePlan: {
+            limit: 1,
+        },
+        hustlerPlan: {
+            limit: 50,
+        },
+        proPlan: {
+            limit: 100,
+        }
+    }
+}
